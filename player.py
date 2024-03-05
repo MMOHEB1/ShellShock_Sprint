6770df211a292b106632ea21fa93b3fd6ba190d1
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-03-05 20:58:51 +0000

    commit 1: just started a new project of a turtle trying to cross the road, trying to make the boiler plate for the player class so the turtle can appear at the bottom of the screen and move upwards on click.

--- player.py
+++ player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.shape("turtle")
+        self.color("black")
+
+
+    def move_up(self):
+        self.left(90)
+        for i in range(4):
+            self.forward(10)

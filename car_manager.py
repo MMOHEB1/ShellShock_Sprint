bcc0bb65b225336cfd7fc9c287e51fcd7dd77615
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-03-13 15:57:26 +0000

    commit 5: managed to make a block with random colours that is position randomly along the y-axis and moves towards the left of the screen.

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,19 @@
+import random
+from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super(CarManager, self).__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_len=3)
+        self.color(COLORS[random.randint(0, 5)])
+        self.setx(200)
+        self.sety(random.randint(-260, 260))
+        self.setheading(180)
+
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from turtle import Screen
 from player import Player, FINISH_LINE_Y
-from car_manager import CarManager
+from car_manager import CarManager, MOVE_INCREMENT
 from scoreboard import Scoreboard
 
 screen = Screen()
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 turtle = Player()
 scoreboard = Scoreboard()
+blocks = CarManager()
 
 
 game_is_on = True
@@ -18,8 +19,11 @@ while game_is_on:
     screen.update()
     screen.onkey(turtle.movement, "Up")
     screen.listen()
+    blocks.forward(MOVE_INCREMENT)
     if turtle.ycor() >= FINISH_LINE_Y:
         screen.update()
         scoreboard.add_score()
         turtle.reset()
+    #     need to increase moving increment
+
 
